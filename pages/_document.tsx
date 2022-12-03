2eceb7d9ac9420b2c04eea11940a51d4b8fb31fc
chrisdrifte <chrisdrifte@gmail.com>
2022-12-03 22:44:04 +0100

    preload fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,26 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="preload"
+          href="/fonts/reenie-beanie-v16-latin-regular.woff2"
+          as="font"
+          type="font/woff2"
+        />
+        <link
+          rel="preload"
+          href="/fonts/reenie-beanie-v16-latin-regular.woff"
+          as="font"
+          type="font/woff"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
