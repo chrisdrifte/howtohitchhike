8452993805f27f0eb784bfb52bd9ab29ce65a08d
chrisdrifte <chrisdrifte@gmail.com>
2022-12-03 07:33:33 +0100

    install analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import { AppProps } from 'next/app'
-import '../styles/index.css'
+import '../styles/index.css';
+
+import { AppProps } from 'next/app';
+
+import { Analytics } from '@vercel/analytics/react';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
