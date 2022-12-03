edd44d9dc377d423840de5836ec2affe40220dfd
chrisdrifte <chrisdrifte@gmail.com>
2022-12-03 21:37:10 +0100

    improve footer copy

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Get the book: Rules of Thumb - How to Hitchhike and Live on the Road
+            Want to keep reading on the road?
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
