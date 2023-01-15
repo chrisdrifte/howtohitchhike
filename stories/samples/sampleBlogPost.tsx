4d6f2e49c443739b28f7ff30ccdef0d58a8da453
chrisdrifte <chrisdrifte@gmail.com>
2023-01-15 15:23:34 +0100

    fix build

--- stories/samples/sampleBlogPost.tsx
+++ stories/samples/sampleBlogPost.tsx
@@ -1,14 +1,15 @@
-import BlogPost from '../../models/BlogPost';
-import ContentType from '../../models/ContentType';
-import { sampleContributor } from './sampleContributor';
-import { sampleDate } from './sampleDate';
-import { samplePost } from './samplePost';
+import BlogPost from "../../models/BlogPost";
+import ContentType from "../../models/ContentType";
+import { sampleContributor } from "./sampleContributor";
+import { sampleDate } from "./sampleDate";
+import { samplePost } from "./samplePost";
 
 export const sampleBlogPost: BlogPost = {
   type: ContentType.BlogPost,
   locale: samplePost.locale,
   slug: samplePost.slug,
   path: samplePost.path,
+  fullPath: samplePost.fullPath,
   title: samplePost.title,
   coverImage: samplePost.coverImage,
   ogImage: samplePost.ogImage,
--- stories/samples/sampleBookExtract.tsx
+++ stories/samples/sampleBookExtract.tsx
@@ -1,12 +1,13 @@
-import BookExtract from '../../models/BookExtract';
-import ContentType from '../../models/ContentType';
-import { samplePost } from './samplePost';
+import BookExtract from "../../models/BookExtract";
+import ContentType from "../../models/ContentType";
+import { samplePost } from "./samplePost";
 
 export const sampleBookExtract: BookExtract = {
   type: ContentType.BookExtract,
   locale: samplePost.locale,
   slug: samplePost.slug,
   path: samplePost.path,
+  fullPath: samplePost.fullPath,
   title: samplePost.title,
   coverImage: samplePost.coverImage,
   ogImage: samplePost.ogImage,
